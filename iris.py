fa4b901c660903de05cf1fed4a1cd90cc999ddd0
Chris B <chris@bargh.com>
2018-02-24 01:34:36 -0600

    Removed unused imports, added scatterplot

--- iris.py
+++ iris.py
@@ -1,24 +1,25 @@
 import pandas as pd
-import sklearn as sk
 import numpy as np
-import sklearn.preprocessing as skp
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_score
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", sep=',', names=['sepel_length', 'sepel_width', 'petal_length', 'petal_width', 'class'])
 
-lb = skp.LabelEncoder()
+lb = LabelEncoder()
 df['class'] = lb.fit_transform(df['class'])
 X, y = df.drop(['class'], axis=1), df['class']
 
 
-outer_cv = StratifiedKFold(10)
-inner_cv = StratifiedKFold(10)
+outer_cv = StratifiedKFold(3)
+inner_cv = StratifiedKFold(3)
 
 
 pipe = Pipeline([
@@ -32,9 +33,8 @@ params = [{
         }, {
             'clf': (LogisticRegression(),),
         }, {
-            'clf': (KNeighborsRegressor(),),
+            'clf': (KNeighborsClassifier(),),
             'clf__n_neighbors': [3, 5, 7],
-            'clf__p': [1, 2],
         }, {
             'clf': (RandomForestClassifier(),),
             'clf__n_estimators': [100, 500, 1000],
@@ -45,11 +45,15 @@ params = [{
             'clf__learning_rate': [.1, 1.0, 3.0],
             }]
 
-clf = GridSearchCV(pipe, param_grid=params, cv=inner_cv, verbose=1)
+clf = GridSearchCV(pipe, param_grid=params, cv=inner_cv, verbose=1) #Inner CV loop
 clf.fit(X, y)
 
-scores = cross_val_score(clf, X, y, cv=outer_cv, scoring='accuracy', verbose=3)
+scores = cross_val_score(clf, X, y, cv=outer_cv, scoring='accuracy', verbose=1) #Outer CV loop
+print(scores.mean())
 
 
+sns.set(style="darkgrid")
+g = sns.pairplot(df, vars=["sepel_length", "sepel_width", "petal_length", "petal_width"], hue="class")
+plt.show()
 
 
